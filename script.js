b15807c2e090ef48272add813cb0170d435f925a
Juan Sack <juannicosack@loscharruas.com.ar>
2023-05-01 14:23:45 -0300

    Added another easter egg

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 let x = 0;
 
-function resetCount() {
-    let x = 0
+function showcat() {
+    catEmoji = document.getElementById('catEmoji')
+    catEmoji.style.display = 'block';
+    setTimeout(() => { catEmoji.style.display = 'none'; }, 1000);
 }
 
-function secret() {
+function cat() {
     x++;
     var soundsList = Array('sfx/meow.mp3', 'sfx/meow2.mp3', 'sfx/meow3.mp3', 'sfx/meow4.mp3', 'sfx/meow5.mp3');
     var sounds = soundsList[Math.floor(Math.random()*soundsList.length)];
@@ -12,4 +14,7 @@ function secret() {
     sound.volume = 0.1;
     sound.play();
     console.log(x)
+    if (x >= '10') {
+       showcat() 
+    }
 }
